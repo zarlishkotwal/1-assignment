a0e25d7278ff11b85be5b9870171e8f1523876a0
zarlishkotwal <you@example.com>
2021-10-25 04:05:56 +0500

    work complete

---
+++ assingment#1/app.js
@@ -0,0 +1,240 @@
+// alert("Hello World")
+
+// var number = 100;
+
+// var number2 = 200 ;
+
+
+// alert('number');
+
+// ///STRING variable/////
+
+
+
+// var name = "zarlish" ;
+
+// alert(name)
+
+
+// var name = "zarlish kotwal";
+// var age = 20;
+
+
+// alert(age)
+
+
+// var num1 = 10.25555
+// alert(num1)
+
+
+//     ///opts//
+
+
+// var num1 = 10 ;
+// var num2 = 5;
+
+// var add = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var per = num1 % num2
+
+// alert(per)
+
+
+
+
+// var name = "zarlish"
+// var num20 = 20
+
+// alert(name + num20)
+
+
+// var num100 = 10;
+// var num200 = "20";
+
+// alert(num100 - num200)
+
+
+// var num1 = 100;
+
+// var num2 = "200"
+
+
+// alert(num1 + num2)
+
+
+
+
+
+
+// alert(add)
+// alert(sub)
+
+
+
+// alert("HELLO CLASS...")
+
+
+// /variables///
+
+
+
+// var name = "zarlish kotwal";
+
+// var age = 20;
+
+
+// alert(name)
+
+
+// /datatypes
+// string
+// Number
+// boolean 
+// undefined
+// null
+
+
+
+
+// var Name = "zarlish"
+
+// var firstName = "zarlish"
+// var last_namE = "kotwal"
+
+// alert(firstName + last_namE)
+
+
+// //MATH EXPRESSTION///
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 200;
+// var num2 = 500;
+
+// var add = num1 % num2
+
+// alert(add)
+
+
+// var result = (2-3) * 10 + 20;
+//             //-1 * 10 +20
+//             //-10 +20
+//             //10
+
+// alert(result)
+
+
+// alert(3)
+// alert(1)
+// alert(2)
+
+
+// var a = "50" ///string
+// var b = 20 ////number
+
+
+// document.write(a + b)
+
+
+
+// var name = "zarlish"
+// var name2 = "kotwal"
+
+// document.write(name + name2)
+
+// var num1 = 30
+// var num2 = 50
+
+// document.write(num1 + num2)
+
+
+// var num = 10;
+
+// // var update_num = num++ //POST
+// var update_num = ++num //PRE
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+// var num = 20;
+
+// var update_num = --num //POST
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+
+
+// var a = 20;
+
+// var result = a++  +   a++ - a  + ++a - a--
+//             //  20   +   21  - 22 + 23  - 23
+//             //  = 19
+
+
+// alert(result)
+
+
+// var a = 15;
+
+// var result = a + a++ - a++ + a  - --a + a-- + a
+//         //    =15 + 15 - 16  +  17 - 16  + 16 + 15
+
+//  alert(result)
+
+
+
+// var a = 16;
+// var result = a++ + a++ + a - a - --a - --a + ++a
+//             //   + 17 
+
+// alert(result)
+
+// var userName  = prompt("User Name")
+
+// document.write(userName)
+
+
+// var firstName = prompt("First Name") 
+// var lastName =  prompt("Last Name")
+
+// document.write(firstName + " " + lastName)
+alert("MARKSHEET")
+
+var name = prompt ("enter your name")
+var schoolName = prompt ("enter your school name")
+var class10 = prompt ("enter your class10")  
+var urdu = + prompt ("enter you urdu subject number")
+var islamic = + prompt ("enter your islamic subject number")
+var english = + prompt ("enter your english subject number")
+var science = + prompt ("enter you science subject number")
+var math = + prompt ("enter your math subject number")
+var obtained_marks=  urdu + islamic + english + science + math ; 
+var percent = obtained_marks/500* 100;
+
+
+
+
+document.write(" full name:" + name);
+document.write("<br> school" + schoolName); 
+document.write ("<br> class10:" +class10);
+document.write("<br> urdu:" + urdu);
+document.write("<br> islamic:" + islamic);
+document.write("<br> english:" + english);
+document.write("<br> science:" + science);
+document.write("<br> math:" + math);
+document.write("<br> obtained mark:" + obtained_marks);  
+document.write("</br> percentage:" + percent);
